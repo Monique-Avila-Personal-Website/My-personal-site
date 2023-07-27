7d1ce00b600d79efbc2249f3650d9505d0762660
Monique <122705027+Moniii333@users.noreply.github.com>
2023-07-26 19:54:43 -0600

    Made some improvements and added contact page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// below for inserting my quote
+// below for inserting my favQuote!
 let quote = document.getElementsByClassName('fav-quote')[0];
 
 const showMyQuote = () => {
@@ -16,15 +16,39 @@ const randColors = () => {
 }
 let intro = document.querySelector('.intro');
 let name = document.querySelector('.name');
+let contacth1 = document.querySelector('.contacth1')
 const setRandColor = () => {
   let rand = randColors();
   intro.style.color = rand;
   name.style.color = rand;
+  // contacth1.style.color =rand;
 }
+
 setRandColor();
 setInterval(() => {
   setRandColor();
-}, 2600);
+}, 3300);
+
+// storing rand colors in local storage for usage across all pages
+colorToElement = (elementId) => {
+  const colors = randColors();
+  localStorage.setItem('colors', colors);
+  const contacth1 = document.querySelector('.' + contacth1);
+  contacth1.style.color = colors;
+}
 
 // hello world below!
 console.log('Hello, world!');
+
+// hamburger
+document.addEventListener('DOMContentLoaded', function() {
+  const menubtn = document.querySelector('.hamburger');
+  const mobileNav = document.querySelector('.mobile-nav');
+
+  menubtn.addEventListener('click', function() {
+  menubtn.classList.toggle('is-active');
+  mobileNav.classList.toggle('is-active');
+});
+});
+
+
